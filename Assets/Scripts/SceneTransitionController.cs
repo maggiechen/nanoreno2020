503fafbe5e409171f55438b1cf5969ef3c2339a5
Maggie Chen <maggieyudanchen@gmail.com>
2020-03-07 20:26:14 -0800

    Add load scene with stub scene transition, and scale UI by canvas

---
+++ Assets/Scripts/SceneTransitionController.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneTransitionController : MonoBehaviour {
+    [SerializeField]
+    private RectTransform imageRectTransform;
+    public void StartTransition(Action sceneLoadCallback) {
+        
+    }
+}
